384363c541e55a7fca3a5bfd7b99263eb23d7c56
Edoardo <edoardo.sangiorgi@edu.unife.it>
2024-09-16 19:49:39 +0200

    basic CRUD functions

--- src/data/data_api.py
+++ src/data/data_api.py
@@ -5,35 +5,44 @@ import os
 ## This is the data file for Data queries
 
 '''
-Index:
+### Index ###
 
-create_file
-create_dir
+-- Subject/Argument operations --
+create_file(argument, path='')
+create_dir(subject)
+read_by_subject(subject)
+read_by_argument(argument, subject)
+
+
+-- Questions Operations--
+
+-- Utils --
+parse(content, subject, argument):
+get_argument_list(subject):
 
 '''
 
+### S U B J E C T / A R G U M E N T  O P E R A T I O N S  #############################################################
 
-##### C R U D  O P E R A T I O N S  ###################################################################################
-
-# --- C r e a t e  F i l e ----------------------------------------------
-def create_file(filename, path=''):
+# --- C r e a t e  F i l e ------------------------------------------------------------------------------------------
+def create_file(argument, path=''):
     '''
-        create a file .txt about a theme
+        create a file .txt (argument)
 
         Input:
-                filename    :   the name of the theme
+                filename    :   the name of the argument
                 str
 
-                path        :   the path (subject) of the theme
+                path        :   the path (subject) of the argument
                 str
     '''
 
-    with open(path + filename, "w") as file:
+    with open(path + argument, "w") as file:
         pass 
 
 
-# --- C r e a t e  D i r e c t o r y  ---------------------------------
-def create_dir(dirname):
+# --- C r e a t e  D i r e c t o r y  --------------------------------------------------------------------------------
+def create_dir(subject):
     '''
         create a directory (a subject)
 
@@ -42,12 +51,127 @@ def create_dir(dirname):
                 str
     '''
 
-    os.makedirs(DATA_PATH + dirname, exist_ok=True) 
+    os.makedirs(DATA_PATH + subject, exist_ok=True) 
 
 
-# --- U p d a t e  Q u e s t i o n  I n  F i l e --------------------
-def update_question(updated_question, question_id):
+# --- R e a d  S u b j e c t --------------------------------------------------------------------------------
+def read_by_subject(subject):
     '''
-        update a 
+        read all the questions in a subject
+
+        Input:
+                subject         :   the subject of the questions
+                str
+
+        Output:
+                <QuestionList>  :   the list of all the questions    
     '''
+
+    argument_list = get_argument_list(subject)
+
+    all_questions = QuestionList()
+    for argument in argument_list:
+        arg_questions = read_by_argument(argument, subject)
+        all_questions.extend(arg_questions)
+
+    return all_questions
+
+
+# --- R e a d  A r g u m e n t  --------------------------------------------------------------------------------------
+def read_by_argument(argument, subject):
+    '''
+        read all the question of an argument
+
+        Input:
+                argument            :   the argument of the questions
+                str
+
+                subject             :   the subject of the questions
+                str
+        
+        Output:
+                <QuestionList>      :   the list of all the questions
+    '''
+
+    path = subject + '/' + argument + '.txt'
+
+    with open(path, 'r') as file:
+        content = file.read()
+    
+    return parse(content, subject, argument)
+
+
+
+### Q U E S T I O N S  O P E R A T I O N S ##############################################################################
+
+# --- S a v e  A r g u m e n t ---
+def save_argument(argument, subject):
     pass
+
+# --- Read Questions ---
+
+# --- Update Question ---
+
+# --- Delete Question ---
+
+
+#### U T I L S ##########################################################################################################
+
+def parse(content, subject, argument):
+    '''
+        it composes the question object read from the file
+
+        Input:
+                content         :   the entire content of the file
+                str
+
+                subject         :   the subject of the questions
+                str
+
+                argument        :   the argument of the questions
+                str
+        
+        Output:
+                question_list   :   the list of all the questions
+                QuestionList
+    '''
+
+    questions = content.split('***')
+    question_list = QuestionList()
+
+    for question in questions:
+        
+        question_attributes = question.split('**:')
+        new_question = Question(
+            question_attributes[0],
+            subject,
+            argument,
+            question_attributes[1],
+            question_attributes[2],
+            question_attributes[3]
+        )
+
+        question_list.append(new_question)
+
+    return question_list
+
+def get_argument_list(subject):
+    '''
+        returns all the arguments in a subject
+
+        Input:
+                subject     :   name of the subject
+                str
+        
+        Output:
+                list<str>   :   the list of all the argumnent in a subject
+    '''
+
+    file_list = os.listdir(subject)
+    args_list = []
+
+    for file in file_list:
+        file = file.split('.')
+        args_list.append[file[0]]
+
+    return args_list
--- src/data/entities/question.py
+++ src/data/entities/question.py
@@ -1,32 +1,195 @@
+import random
 
-### Q U E S T I O N  C L A S S  ########################################################################################
-class Question():
+
+### Q U E S T I O N  C L A S S ########################################################################################
+class Question:
+    '''
+        It represents the question in the application.
+
+        Each question has:
+        - just a subject
+        - just an argument
+
+        Specfically:
+        - each subject has one or more arguments
+        - each argument has one or more questions
+    '''
 
     def __init__(
         self,
         id,
-        subject = '',
-        argument = '',
-        text = '',
-        ans = '',
-        deleted = True,
+        subject='',
+        argument='',
+        text='',
+        ans='',
+        deleted=False,
     ):
+        self.id = id
+        self.subject = subject
+        self.argument = argument
+        self.text = text
+        self.ans = ans
+        self.deleted = deleted
 
-      self.id = id
-      self.subject = subject
-      self.argument = argument
-      self.text = text
-      self.ans = ans
-      self.deleted = deleted
+
+    def __repr__(self):
+        '''
+            returns a string representation of the object Question
+
+            Output:
+                    <string>    :   key,value-like
+                                    ex:
+                                    Question(id=1, subject=Math, argument=Functions, deleted=False)
+        '''
+        return f"Question(id={self.id}, subject='{self.subject}', argument='{self.argument}', deleted={self.deleted})"
 
 
-##### Q U E S T I O N  L I S T  C L A S S  ###############################################################################
+
+
+##### Q U E S T I O N  L I S T  C L A S S ###############################################################################
 class QuestionList(list):
-   
-    # --- A d d ---
 
-    # --- D e l e t e ---
+    def __init__(self):
+        super().__init__()
+        
 
-    # --- U p d a t e ---
 
-    pass
+    # --- C r e a t e -----------------------------------------------------------------------------------------------------
+
+    def append(self, new_question):
+        '''
+            add a question in the question list
+
+            Input:
+                    new_question    :   question to add
+                    Question
+        '''
+
+        if isinstance(new_question, Question):
+            super().append(new_question)
+
+        else:
+            raise TypeError("The instance passed is not a Question instance")
+        
+    
+    def extend(self, new_question_list):
+        '''
+            extend the self list with all the elements of the passed list
+
+            Input:
+                    new_question_list   :   the list to ad to the self
+                    QuestionList
+        '''
+
+        if isinstance(new_question_list, QuestionList):
+            super().extend(new_question_list)
+
+        else:
+            raise TypeError('The data passed are not  QuestionList instances')
+
+
+
+    # --- R e a d ----------------------------------------------------------------------------------------------------------
+    
+    def get_by_id(self, id):
+        '''
+            return a question by id
+
+            Input:
+                    id          :   id of the question
+                    int
+
+            Output:
+                    question    :   found question
+                    Question
+
+                    or
+
+                    None        :   question not found
+        '''
+
+        for question in self:
+            if question.id == id:
+                return question
+    
+        return None
+
+
+    def get_by_subject(self, subject):
+        '''
+            returns all the question of a subject
+
+            Input:
+                    subject         :   the subject of the question
+                    str
+
+            Output:
+                    list<Question>  :   the list of the question that belong to a subject
+                                        the argument of the single questions could be different
+        '''
+
+        return [question for question in self if question.subject == subject]
+    
+
+    def get_by_argument(self, argument):
+        '''
+            returns all the question of an argument
+
+            Input:
+                    argument         :   the argument of the question
+                    str
+
+            Output:
+                    <QuestionList>  :   the list of the question that belong to an argument
+                                        the subject of the questions will be the same
+        '''
+
+        return [question for question in self if question.argument == argument]
+
+
+
+    # --- U p d a t e -------------------------------------------------------------------------------------------------
+
+    def update_question(self, id, **kwargs):
+        '''
+            update the question with the specified id
+
+            Input:
+                    id  :   the id of the question to update
+                    int           
+        '''
+
+        question = self.get_by_id(id)
+        if question:
+            for key, value in kwargs.items():
+                if hasattr(question, key):
+                    setattr(question, key, value)
+        else:
+            print(f"No question found with ID: {id}")
+
+
+
+    # --- D e l e t e ------------------------------------------------------------------------------------------------
+
+    def delete_by_id(self, id):
+        '''
+            remove the question by the id (set deleted as True)
+
+            Input:
+                    id  :   id of the question
+                    int
+        '''
+
+        question = self.get_by_id(id)
+        if question:
+            question.deleted = True
+        else:
+            print(f"No question found with ID: {id}")
+
+    
+    # --- S h u f f l e -------------------------------------------------------------------------------------------
+    def shuffle(self):
+        '''
+            shuffle the original list
+        '''
+        random.shuffle(self)
